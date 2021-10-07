16cb54e6d45d3cab9ac939655a5bf7d6c5486dc6
Li Chai <67728157+jxcharlie1991@users.noreply.github.com>
2021-10-07 15:00:41 +1100

    Delete RobotChallenge/Robot-Challenge-Basic directory

--- RobotChallenge/Robot-Challenge-Basic/Robot-Challenge-Basic/Enumerations.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Robot_Challenge_Basic
-{
-    class Enumerations
-    {
-        public enum Direction { east, south, west, north }
-        public enum Command { place, move, left, right, report, exit, unrecognise }
-    }
-}
