d2461f963f351f30b1a62a3751e17ae2b23570ff
Li Chai <67728157+jxcharlie1991@users.noreply.github.com>
2021-10-07 21:51:56 +1100

    Update Human.cs

--- RobotChallenge/Robot-Challenge-Extended/Robot-Challenge-Extended/Human.cs
+++ RobotChallenge/Robot-Challenge-Extended/Robot-Challenge-Extended/Human.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace Robot_Challenge_Extended
@@ -22,11 +22,12 @@ namespace Robot_Challenge_Extended
         //thus separate the first command checking from the command checking would optimise the efficiency, and user would be more clear to input command.
         public static void AnalyseFirstCommand(string userInput)
         {
-            if (userInput.ToLower() == "exit")
+            userInput = userInput.ToLower();
+            if (userInput == "exit")
             {
                 Comm = Enumerations.Command.exit;
             }
-            else if (userInput.ToLower().StartsWith("place "))
+            else if (userInput.StartsWith("place "))
             {
                 string[] placeLoc = userInput.Substring(6).Split(',');
                 if (int.TryParse(placeLoc[0], out int x) && int.TryParse(placeLoc[1], out int y) && Enum.IsDefined(typeof(Enumerations.Direction), placeLoc[2]))
@@ -61,7 +62,8 @@ namespace Robot_Challenge_Extended
         //Separate the validity checking would make the project easier to modify and update.
         public static void AnalyseCommand(string userInput)
         {
-            if (userInput.ToLower().StartsWith("place "))
+            userInput = userInput.ToLower();
+            if (userInput.StartsWith("place "))
             {
                 string[] commLoc = userInput.Substring(6).Split(',');
                 if (int.TryParse(commLoc[0], out int x) && int.TryParse(commLoc[1], out int y) && Enum.IsDefined(typeof(Enumerations.Direction), commLoc[2]))
@@ -86,7 +88,7 @@ namespace Robot_Challenge_Extended
                     Comm = Enumerations.Command.unrecognise;
                 }
             }
-            else if (userInput.ToLower().StartsWith("robot ") && int.TryParse(userInput.Substring(6), out int commSerial))
+            else if (userInput.StartsWith("robot ") && int.TryParse(userInput.Substring(6), out int commSerial))
             {
 
                 if (commSerial > 0 && commSerial <= DeclareSerial)
