7655427d6c7e9fa28dcbd423c66a3363fd4e56f8
Li Chai <67728157+jxcharlie1991@users.noreply.github.com>
2021-10-07 14:21:29 +1100

    Add files via upload

---
+++ RobotChallenge/Robot-Challenge-Extended/Robot-Challenge-Extended/Enumerations.cs
@@ -0,0 +1,8 @@
+﻿namespace Robot_Challenge_Extended
+{
+    class Enumerations
+    {
+        public enum Direction { east, south, west, north }
+        public enum Command { place, change, move, left, right, report, exit, unrecognise }
+    }
+}
